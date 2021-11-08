f15b90c0fa301b64ec0144e3f8cde7ffc8a3d3f0
Hariom-agg <74823540+Hariom-agg@users.noreply.github.com>
2021-11-08 18:44:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+function bt1()
+{
+    window.location = "Screen_living.html";
+}
+
+function bt2()
+{
+    window.location = "Screen_AC.html";
+}
+
+function  bt3()
+{
+    window.location = "Screen_TV.html";
+}
+
+
+
+function  bt4()
+{
+    window.location = "Screen_balcani.html";
+}

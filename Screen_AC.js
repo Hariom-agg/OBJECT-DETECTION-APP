913672873ad0eeb352ed1fdd7bd99392e0130ae2
Hariom-agg <74823540+Hariom-agg@users.noreply.github.com>
2021-11-13 21:40:59 +0530

    Add files via upload

---
+++ Screen_AC.js
@@ -0,0 +1,47 @@
+
+Status = "";
+
+
+function preload()
+{
+    img = loadImage('IMG20211026163339.jpg');
+}
+
+function setup ()
+{
+   canvas = createCanvas(400,300);
+   canvas.center();
+   objectDetector = ml5.objectDetector("cocossd" , modelLoaded);
+   document.getElementById("status").innerHTML = "Status : Detecting Objects";
+
+}
+
+function modelLoaded()
+{
+    console.log("MODELLOADED!")
+   Status = "true";
+   objectDetector.detect(img , gotResults);
+}
+
+function gotResults(error , results)
+{
+   if(error){
+       console.log(error);
+   }
+   else{
+    console.log(results);
+   }
+
+   
+}
+
+function draw()
+{
+
+}
+
+function back_ac ()
+{
+    window.location = "index.html";
+}
+

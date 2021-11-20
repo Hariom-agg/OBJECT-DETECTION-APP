5a7c7f2831292e4e84f60056eb5cbe6676e3e4fb
Hariom-agg <74823540+Hariom-agg@users.noreply.github.com>
2021-11-20 10:45:57 +0530

    Add files via upload

--- Screen_AC.js
+++ Screen_AC.js
@@ -1,5 +1,6 @@
-
+img = "";
 Status = "";
+object = [];
 
 
 function preload()
@@ -9,7 +10,7 @@ function preload()
 
 function setup ()
 {
-   canvas = createCanvas(400,300);
+   canvas = createCanvas(500,300);
    canvas.center();
    objectDetector = ml5.objectDetector("cocossd" , modelLoaded);
    document.getElementById("status").innerHTML = "Status : Detecting Objects";
@@ -30,6 +31,7 @@ function gotResults(error , results)
    }
    else{
     console.log(results);
+    object = results;
    }
 
    
@@ -38,14 +40,30 @@ function gotResults(error , results)
 
 function draw()
 {
+    image(img , 0, 0, 500 , 300);
 
+    if(Status != "")
+    {
+        
+        for(i = 0; i<object.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("objects-detect-live").innerHTML =  "There are 2 big objects in this image from which " + object.length + " objects are detected";
+            fill("#FF0000");
+            percent = floor(object[i].confidence * 100)+"%";
+            console.log(percent);
+            console.log(object[i].label);
+            textSize(20);
+            text(object[i].label ,object[i].x + 15 , object[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x , object[i].y , object[i].width , object[i].height);
+        }
+    }
 }
 
 
-function back_ac ()
+function back ()
 {
     window.location = "index.html";
 }
-
-
-
--- Screen_balcani.js
+++ Screen_balcani.js
@@ -1,24 +1,25 @@
-
+img = "";
 Status = "";
+object = [];
 
 
 function preload()
 {
-    img = loadImage('e.jpeg');
+    img = loadImage('e.jpg');
 }
 
 function setup ()
 {
-   canvas = createCanvas(400,300);
+   canvas = createCanvas(500,300);
    canvas.center();
    objectDetector = ml5.objectDetector("cocossd" , modelLoaded);
-   document.getElementById("status").innerHTML = "Status = Detecting Objects";
+   document.getElementById("status").innerHTML = "Status : Detecting Objects";
 
 }
 
 function modelLoaded()
 {
-   console.log("MODELLOADED!")
+    console.log("MODELLOADED!")
    Status = "true";
    objectDetector.detect(img , gotResults);
 }
@@ -30,31 +31,39 @@ function gotResults(error , results)
    }
    else{
     console.log(results);
+    object = results;
    }
 
    
 }
 
 
-
-
 function draw()
 {
+    image(img , 0, 0, 500 , 300);
 
+    if(Status != "")
+    {
+        
+        for(i = 0; i<object.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("objects-detect-live").innerHTML =  "There are 2 big objects in this image from which " + object.length + " objects are detected";
+            fill("#FF0000");
+            percent = floor(object[i].confidence * 100)+"%";
+            console.log(percent);
+            console.log(object[i].label);
+            textSize(20);
+            text(object[i].label ,object[i].x + 15 , object[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x , object[i].y , object[i].width , object[i].height);
+        }
+    }
 }
 
 
-
-
-
-
-
-
-
-
-
-function back_ac ()
+function back ()
 {
     window.location = "index.html";
 }
-
